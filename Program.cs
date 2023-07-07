a6497afd82611c0ae3293ad731137ff15561ef9b
RafDevOfficial <83568542+RafCodes-del@users.noreply.github.com>
2023-07-07 18:40:06 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -22,15 +22,7 @@ class FunnyLanguage
             return;
         }
 
-        if (words[1] != "I" || words[2] != "Want" || words[3] != "to" || words[4] != "Make" || words[5] != "a" ||
-            words[6] != "text" || words[7] != "that" || words[8] != "appeared" || words[9] != "and" || words[10] != "says" ||
-            words[11] != "(" || !words[12].EndsWith(")"))
-        {
-            Console.WriteLine("Invalid phrase. The phrase should follow the format: 'Hey I Want to Make a text that appeared and says (Text Specified)'");
-            return;
-        }
-
-        string text = phrase.Substring(phrase.IndexOf('(') + 1, phrase.LastIndexOf(')') - phrase.IndexOf('(') - 1);
+        string text = phrase.Substring(phrase.LastIndexOf('(') + 1, phrase.Length - phrase.LastIndexOf('(') - 2);
         Console.WriteLine(text);
     }
 }
